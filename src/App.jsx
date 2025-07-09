fc185a6a39ca98de4df751b81856d3861d505599
Alking <alrey2635@gmail.com>
2025-07-09 12:59:37 +0400

    modification

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import Apropos from "./components/Apropos";
-
+import Navbar from "./components/Navbar";
+import Accueil from "./components/Accueil";
 function App() {
   return (
     <div>
+      <Navbar />
+      <Accueil />
       <Apropos />
      </div>
   );
