d2c09b1b9eb4f7f14173dc05c135d817d8a54f62
valisoa01 <valisoa.rafanomezantsoa@student.passerellesnumeriques.org>
2025-07-10 14:51:52 -1200

    partie 1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,20 @@
-import "./Navbar.css"
-export default function Navbar() {
+ import Logo from '../assets/images/logo-hei.png'
+ export default function Navbar() {
     return(
-        <nav class="bg-blue-900 h-20 flex items-center justify-around">
+        <nav class="bg-[#001948] h-20 flex items-center justify-around">
             <div class="logo">
-                <img src= ""alt="Logo HEI" class="h-12" />
+                <img src={Logo} alt="Logo HEI" class="h-12 cursor-pointer hover:opacity-90 transition-all" />
             </div>
     
-            <div class="flex gap-6">
-                <a href="#" class="text-white text-xl hover:text-yellow-400 transition duration-300 ease-in-out">Accueil</a>
-                <a href="#" class="text-white text-xl hover:text-yellow-400 transition duration-300 ease-in-out">Actualité</a>
-                <a href="#" class="text-white text-xl hover:text-yellow-400 transition duration-300 ease-in-out">Bourse d'études</a>
-                <a href="#" class="text-white text-xl hover:text-yellow-400 transition duration-300 ease-in-out">Inscription</a>
-                <button class="bg-yellow-300/80 w-24 h-10 rounded-md cursor-pointer hover:bg-yellow-300 hover:-translate-y-1 hover:text-black transition-all duration-300 ease-in-out border-none">Intranet</button>
+            <div class="flex gap-6 items-center">
+                <a href="#" class="text-[#DFA408] text-[16px] font-bold cursor-pointer transition duration-300 ease-in-out">ACCUEIL</a>
+                <a href="#" class="text-white text-[16px] hover:text-[#DFA408] font-bold cursor-pointer transition duration-300 ease-in-out">ACTUALITÉ</a>
+                <a href="#" class="text-white text-[16px] hover:text-[#DFA408] cursor-pointer font-bold transition duration-300 ease-in-out">BOURSE D'ÉTUDES</a>
+                <a href="#" class="text-white text-[16px] hover:text-[#DFA408] cursor-pointer font-bold transition duration-300 ease-in-out">INSCRIPTION</a>
+                <button class="bg-[#DFA408] transition-all py-2 px-4 rounded-md cursor-pointer hover:text-[#001948] hover:-translate-y-1  text-white font-bold duration-300 ease-in-out border-none text-[16px]">INTRANET</button>
             </div>
+            {/* bg-[#DFA408] */}
+            {/* text-[#001948] */}
         </nav>
     )
 
